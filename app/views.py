2bec139a920e5765ca4c8aab8b34859bfd6d2a89
OlegGibadulin <O.Gibadulin@yandex.ru>
2020-04-01 23:54:38 +0300

    Add home page

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+def index(request):
+    return render(request, 'index.html', {})
+
+def question(request):
+    return render(request, 'base.html', {})
 
 # Create your views here.
