c7185a0dc7a1dfc1f45aa1043201a0528177e1f3
OlegGibadulin <O.Gibadulin@yandex.ru>
2020-04-03 15:51:39 +0300

    Add question template

--- app/views.py
+++ app/views.py
@@ -1,19 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
+def get_questions():
+    questions = {}
+    for i in range(1, 30):
+        questions.update({i: {
+            'title': f'Title #{i}',
+            'text': f'Text #{i}',
+            'id': i
+        }})
+    return questions
+
 def index(request):
+    return render(request, 'index.html', {
+        'questions': get_questions().values()
+    })
+
+def hot(request):
     return render(request, 'index.html', {})
 
-def question(request):
-    return render(request, 'question.html', {})
+def tag(request, tag_name):
+    return render(request, 'index.html', {})
 
-def ask(request):
-    return render(request, 'ask.html', {})
+def question(request, qid):
+    question = get_questions().get(qid)
+    return render(request, 'question.html', {
+        'question': question
+    })
 
 def login(request):
     return render(request, 'login.html', {})
 
 def signup(request):
     return render(request, 'signup.html', {})
+
+def ask(request):
+    return render(request, 'ask.html', {})
 
 # Create your views here.
--- askme/urls.py
+++ askme/urls.py
@@ -20,9 +20,11 @@ from app import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.index),
-    path('question/', views.question),
-    path('ask/', views.ask),
-    path('login/', views.login),
-    path('signup/', views.signup),
+    path('', views.index, name='index'),
+    path('hot/', views.hot, name='hot'),
+    path('tag/<str:name>', views.tag, name='tag'),
+    path('question/<int:qid>/', views.question, name='question'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('ask/', views.ask, name='ask'),
 ]
