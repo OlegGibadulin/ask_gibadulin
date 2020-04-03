801042c27e6f935260cdd722a4cb08bc606c8e8e
OlegGibadulin <O.Gibadulin@yandex.ru>
2020-04-03 18:32:41 +0300

    Provide data for question template

--- app/views.py
+++ app/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
-def get_questions():
-    questions = {}
-    for i in range(1, 30):
-        questions.update({i: {
-            'title': f'Title #{i}',
-            'text': f'Text #{i}',
-            'id': i
+check = True
+questions = {}
+for i in range(1, 10):
+    answers = {}
+    for j in range(1, 3):
+        check = not check
+        answers.update({j: {
+            'text': f'Answer #{j} of question #{i}',
+            'rating': j * 100,
+            'correct_answer': check
         }})
-    return questions
+    questions.update({i: {
+        'title': f'Title #{i}',
+        'text': f'Text #{i}',
+        'id': i,
+        'answers': answers,
+        'answers_count': 2,
+        'rating': i * 100,
+        'tags': ['python', 'django', 'bootstrap', 'html']
+    }})
 
 def index(request):
     return render(request, 'index.html', {
-        'questions': get_questions().values()
+        'questions': questions.values()
     })
 
 def hot(request):
@@ -23,9 +34,12 @@ def tag(request, tag_name):
     return render(request, 'index.html', {})
 
 def question(request, qid):
-    question = get_questions().get(qid)
+    question = questions.get(qid)
+    answers = question['answers']
+    print(answers.values())
     return render(request, 'question.html', {
-        'question': question
+        'question': question,
+        'answers': answers.values()
     })
 
 def login(request):
